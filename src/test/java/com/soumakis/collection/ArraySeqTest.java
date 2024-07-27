2d973eee64282852001f278fad99ae8177f79a55
Vasilis Soumakis <billsoumakis@gmail.com>
2024-07-28 00:49:54 +0300

    Adding tests for respecting monad laws on ArraySeq

--- src/test/java/com/soumakis/collection/ArraySeqTest.java
+++ src/test/java/com/soumakis/collection/ArraySeqTest.java
@@ -2,6 +2,7 @@ package com.soumakis.collection;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
+import java.util.function.Function;
 import org.junit.jupiter.api.Test;
 
 public class ArraySeqTest {
@@ -60,7 +61,7 @@ public class ArraySeqTest {
     ArraySeq<Integer> seq = ArraySeq.of(1, 2, 3);
     ArraySeq<Integer> seq2 = seq.prepend(0);
     assertEquals(4, seq2.length());
-    assert(seq2.equals(ArraySeq.of(0, 1, 2, 3)));
+    assert (seq2.equals(ArraySeq.of(0, 1, 2, 3)));
   }
 
   @Test
@@ -68,7 +69,7 @@ public class ArraySeqTest {
     ArraySeq<Integer> seq = ArraySeq.of(1, 2, 3);
     ArraySeq<Integer> seq2 = seq.map(x -> x * 2);
     assertEquals(3, seq2.length());
-    assert(seq2.equals(ArraySeq.of(2, 4, 6)));
+    assert (seq2.equals(ArraySeq.of(2, 4, 6)));
   }
 
   @Test
@@ -76,7 +77,7 @@ public class ArraySeqTest {
     ArraySeq<Integer> seq = ArraySeq.of(1, 2, 3);
     ArraySeq<Integer> seq2 = seq.flatMap(x -> ArraySeq.of(x, x * 2));
     assertEquals(6, seq2.length());
-    assert(seq2.equals(ArraySeq.of(1, 2, 2, 4, 3, 6)));
+    assert (seq2.equals(ArraySeq.of(1, 2, 2, 4, 3, 6)));
   }
 
   @Test
@@ -84,7 +85,7 @@ public class ArraySeqTest {
     ArraySeq<Integer> seq = ArraySeq.of(1, 2, 3);
     ArraySeq<Integer> seq2 = seq.update(1, 4);
     assertEquals(3, seq2.length());
-    assert(seq2.equals(ArraySeq.of(1, 4, 3)));
+    assert (seq2.equals(ArraySeq.of(1, 4, 3)));
   }
 
   @Test
@@ -92,7 +93,7 @@ public class ArraySeqTest {
     ArraySeq<Integer> seq = ArraySeq.of(1, 2, 3);
     ArraySeq<Integer> seq2 = seq.delete(2);
     assertEquals(2, seq2.length());
-    assert(seq2.equals(ArraySeq.of(1, 2)));
+    assert (seq2.equals(ArraySeq.of(1, 2)));
   }
 
   @Test
@@ -100,7 +101,7 @@ public class ArraySeqTest {
     ArraySeq<Integer> seq = ArraySeq.of(1, 2, 3, 4, 5);
     ArraySeq<Integer> seq2 = seq.filter(x -> x % 2 == 0);
     assertEquals(2, seq2.length());
-    assert(seq2.equals(ArraySeq.of(2, 4)));
+    assert (seq2.equals(ArraySeq.of(2, 4)));
   }
 
   @Test
@@ -111,6 +112,37 @@ public class ArraySeqTest {
     assertEquals(1, list.get(0));
     assertEquals(2, list.get(1));
     assertEquals(3, list.get(2));
+  }
+
+  @Test
+  void testLeftIdentity() {
+    ArraySeq<Integer> unitSeq = ArraySeq.of(5);
+    Function<Integer, ArraySeq<Integer>> f = x -> ArraySeq.of(x * 2);
+    ArraySeq<Integer> result1 = unitSeq.flatMap(f);
+    ArraySeq<Integer> result2 = f.apply(5);
+
+    assertEquals(result1, result2);
+  }
+
+  @Test
+  void testRightIdentity() {
+    ArraySeq<Integer> seq = ArraySeq.of(1, 2, 3);
+    Function<Integer, ArraySeq<Integer>> unit = ArraySeq::of;
+    ArraySeq<Integer> result = seq.flatMap(unit);
+
+    assertEquals(result, seq);
+  }
+
+  @Test
+  void testAssociativity() {
+    ArraySeq<Integer> seq = ArraySeq.of(1, 2, 3);
+    Function<Integer, ArraySeq<Integer>> f = x -> ArraySeq.of(x, x * 2);
+    Function<Integer, ArraySeq<Integer>> g = x -> ArraySeq.of(x + 1);
+
+    ArraySeq<Integer> result1 = seq.flatMap(f).flatMap(g);
+    ArraySeq<Integer> result2 = seq.flatMap(x -> f.apply(x).flatMap(g));
+
+    assertEquals(result1, result2);
   }
 
 }
